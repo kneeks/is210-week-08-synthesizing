60458ed0564e00071f7f573fd9d2451cf6457bde
Vagrant User <mr.nikovaccaro@gmail.com>
2015-10-19 06:06:15 +0000

    testing task 2 fixed between nums

--- task_02.py
+++ task_02.py
@@ -10,7 +10,7 @@ DAYS = ('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat')
 DAY = raw_input(QUESTION)[:3].lower()
 TIME = raw_input(QUESTION2)
 if TIME.isdigit():
-    if int(TIME[:2]) in range(25) and int(TIME[2:]) in range(60):
+    if int(TIME[:2]) is 0 <= TIME <= 24 and int(TIME[2:]) in 0 <= TIME <= 60:
         SNOOZE = True if (DAY == 'sat' or DAY == 'sun') or TIME < 600 else False
         if not SNOOZE:
             print 'Beep! Beep! Beep! Beep! Beep!'
