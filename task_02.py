194c0e2b9ea9a7178b42e536e2914091b6e9653c
Vagrant User <mr.nikovaccaro@gmail.com>
2015-10-19 06:33:35 +0000

    confusing this snooze = true value

--- task_02.py
+++ task_02.py
@@ -15,6 +15,7 @@ MIN = int(TIME[2:])
 if (0 <= HR <= 24) and (0 <= MIN < 60):
     SNOOZE = True if (DAY == 'sat' or DAY == 'sun') or TIME < 600 else False
     if not SNOOZE:
+        SNOOZE = True
         print 'Beep! Beep! Beep! Beep! Beep!'
 else:
     print 'Error, re-evaluate.'
